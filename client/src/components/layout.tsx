b42898f68900e4ff9ed731963343bacdd794509b
Sixten <landysixten@gmail.com>
2022-05-05 14:48:41 +0200

    Update layout and UI

--- client/src/components/layout.tsx
+++ client/src/components/layout.tsx
@@ -1,8 +1,29 @@
-function Header() {
+import Link from 'next/link';
+
+const SearchBar = () => (
+  <div className='bg-white p-2 rounded'>
+    <form action='/' method='get'>
+      <input
+        type='text'
+        id='header-search'
+        placeholder='Search for products...'
+        name='s'
+        className='w-96'
+      />
+    </form>
+  </div>
+);
+
+const Header = () => {
   return (
-    <header className='text-center bg-black h-[10vh] text-2xl text-white flex justify-center mb-6'>
+    <header className='bg-black h-[10vh] text-2xl text-white flex justify-center'>
       <div className='w-[85vw] md:w-[75vw] max-w-[910px] h-[10vh] flex items-center justify-between'>
-        <h1 className='font-bold'>Logo</h1>
+        <Link href='/'>
+          <h1 className='font-bold hover:cursor-pointer transition-all hover:scale-110'>
+            LOGO
+          </h1>
+        </Link>
+        <SearchBar />
         <div className='flex text-xl'>
           <div className='h-[10vh] flex ml-8 items-center gap-2 transition-all'>
             <svg
@@ -20,28 +41,68 @@ function Header() {
               />
             </svg>
           </div>
-          <div className='h-[10vh] flex ml-8 items-center gap-2 transition-all'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='h-6 w-6'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke='currentColor'
-              strokeWidth='2'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z'
-              />
-            </svg>
-          </div>
+          <Link href='/cart'>
+            <div className='h-[10vh] flex ml-8 items-center gap-2 transition-all hover:cursor-pointer hover:scale-110'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-6 w-6'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+                strokeWidth='2'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z'
+                />
+              </svg>
+            </div>
+          </Link>
         </div>
       </div>
     </header>
   );
-}
+};
 
+const Footer = () => {
+  return (
+    <footer className='bg-black flex justify-center items-center py-10'>
+      <div className='w-[85vw] md:w-[75vw] max-w-[910px] flex flex-col items-center pt-4'>
+        <div className='text-white w-full basis-1/2 flex flex-col justify-center'>
+          <h1 className='text-2xl font-bold'>LOGO</h1>
+          <hr className='my-4' />
+        </div>
+        <div className='w-full flex flex-row basis-1/2 text-white'>
+          <div className='basis-1/3'>
+            <h3 className='text-xl font-medium mb-6'>Sociala medier</h3>
+            <ul className='flex flex-row gap-6'>
+              <li>Facebook</li>
+              <li>Twitter</li>
+              <li>Instagram</li>
+            </ul>
+          </div>
+          <div className='basis-1/3'>
+            <h3 className='text-xl font-medium mb-6'>Om oss</h3>
+            <ul>
+              <li>rad 1</li>
+              <li>rad 2</li>
+              <li>rad 3</li>
+            </ul>
+          </div>
+          <div className='basis-1/3'>
+            <h3 className='text-xl font-medium mb-6'>Kundservice</h3>
+            <ul>
+              <li>rad 1</li>
+              <li>rad 2</li>
+              <li>rad 3</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 interface Props {
   children: React.ReactNode;
 }
@@ -50,7 +111,8 @@ export default function Layout({ children }: Props) {
   return (
     <>
       <Header />
-      <main className='flex justify-center'>{children}</main>
+      <main className='flex justify-center my-10'>{children}</main>
+      <Footer />
     </>
   );
 }
