83ce7f8ad293780127d646834e5a54482bbbf293
Sixten <landysixten@gmail.com>
2022-05-09 14:05:39 +0200

    Add ioredis and connect-redis

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,23 +4,43 @@ import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { ProductResolver } from './resolvers/product';
 import { UserResolver } from './resolvers/user';
-import { conn } from './constants';
+import { conn, __prod__ } from './constants';
+import Redis from 'ioredis';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
 
 const PORT = 4000;
 
 const main = async () => {
   conn.initialize();
 
+  const app = express();
+  const RedisStore = connectRedis(session);
+  const redisClient = new Redis('127.0.0.1:6379');
+  app.use(
+    session({
+      name: 'qid',
+      store: new RedisStore({ client: redisClient as any, disableTouch: true }),
+      saveUninitialized: false,
+      resave: false,
+      secret: 'ntig123!',
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // max age: 10 years
+        httpOnly: true,
+        sameSite: 'lax',
+        secure: __prod__,
+      },
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [ProductResolver, UserResolver],
       validate: false,
     }),
+    context: ({ req, res }) => ({ req, res }),
   });
-
   await apolloServer.start();
-
-  const app = express();
   apolloServer.applyMiddleware({ app });
 
   app.listen(PORT, () => {
