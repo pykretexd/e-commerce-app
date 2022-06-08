d02e8bf49e046f26689b596090bf5b7f4d92deb9
Sixten <landysixten@gmail.com>
2022-06-08 14:58:32 +0200

    Revert "Merge pull request #1 from sixtenl/development"
    This reverts commit 88fed0dfc3b0d633988acc10d4f8490077f5152e, reversing
    changes made to 2ff20fad4b8c96cfe633909c20b54fdfe527dabd.

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import { FunctionComponent, useContext } from 'react';
 import Stripe from 'stripe';
 import {
@@ -21,26 +20,24 @@ const Card: FunctionComponent<CardProps> = ({ price }) => {
   };
 
   return (
-    <div className='flex flex-col p-2 mx-8 my-4 max-w-[175px] min-h-[260px] max-h-[355px] lg:max-h-[510px] lg:max-w-[300px] md:min-h-[335px] hover:cursor-pointer'>
-      <Link href={`/${price.id}`}>
+    <>
+      <div className='flex flex-col mx-8 my-4 max-w-[175px] min-h-[260px] max-h-[355px] lg:max-h-[510px] lg:max-w-[300px] md:min-h-[335px]'>
         <img
           className='w-3/4 h-3/4 object-center object-cover self-center'
           src={getProductImage(price.product)}
         />
-      </Link>
-      <Link href={`/${price.id}`}>
         <div className='flex flex-col'>
           <p className='sm:col-span-2'>{getProductName(price.product)}</p>
           <p>{getPriceTotal(price)} kr</p>
+          <button
+            onClick={() => addToCart(price)}
+            className='relative p-2 my-2 w-full bg-green-500 text-white rounded-3xl self-center'
+          >
+            Lägg i varukorg
+          </button>
         </div>
-      </Link>
-      <button
-        onClick={() => addToCart(price)}
-        className='relative p-2 my-2 w-full border-2 border-black font-bold rounded-3xl self-center'
-      >
-        Lägg i varukorg
-      </button>
-    </div>
+      </div>
+    </>
   );
 };
 
--- utils/computed.ts
+++ utils/computed.ts
@@ -1,3 +1,5 @@
+import { Stripe } from 'stripe';
+
 export function getProductName(product: any) {
   return product?.name;
 }
